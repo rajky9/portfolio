da23cc673f891bb5578893e0a768ae894706f81c
Kristian Demko <demkokristian@gmail.com>
2024-08-15 02:21:51 +0200

    odstranenie full-stack web z Main

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -9,12 +9,12 @@ const Main = () => {
         <h1 className="py-3 text-gray-700">
           Hi, I&#39;m <span className="text-indigo-600">Kristian</span>
         </h1>
-        <h1 className="text-gray-700 py-2">A Full-Stack Web Developer</h1>
+        <h1 className="text-gray-700 py-2">A Full-Stack Developer</h1>
         <p className="text-lg text-gray-600 mt-3 shadow-sm  tracking-widest">
           LET&#39;S BUILD SOMETHING TOGETHER
         </p>
         <p className="max-w-[400px] text-sm w-full leading-6 py-4 text-gray-600">
-          I’m focused on building responsive full-stack web applications.
+          I’m focused on building responsive full-stack applications.
         </p>
         <div className="flex md:gap-6 gap-3 mt-2">
           <div className="p-3 md:scale-125 scale-110 shadow-lg shadow-gray-400 cursor-pointer hover:scale-90 md:hover:scale-105 rounded-full duration-200">
